eb16dd44dca924a114467a715df3281868ecceb4
Towhidul Islam <towhidul.islam5797@gmail.com>
2026-06-20 15:26:42 +0600

    Feat: M3-S1
    -Add MainMenu scene,
    -SceneLoader,
    -fix GemHunter hardcoded scene index bug,
    -add cross-app back navigation

--- Assets/GemHunterMatch/Scripts/UI/UIHandler.cs
+++ Assets/GemHunterMatch/Scripts/UI/UIHandler.cs
@@ -168,7 +168,7 @@ namespace Match3
             {
                 FadeOut(() =>
                 {
-                    SceneManager.LoadScene(1, LoadSceneMode.Single); 
+                    SceneManager.LoadScene("Main", LoadSceneMode.Single);
                 });
             };
             
@@ -197,7 +197,7 @@ namespace Match3
                 FadeOut(() =>
                 {
                     ToggleSettingMenu(false);
-                    SceneManager.LoadScene(1, LoadSceneMode.Single); 
+                    SceneManager.LoadScene("Main", LoadSceneMode.Single);
                 });
             };
 
